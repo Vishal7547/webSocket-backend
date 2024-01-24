8a6b95b0049125d3d6f274a46b7b9d5a896cbd2e
Vishal kumar <vishalk80269@gmail.com>
2024-01-24 15:42:21 +0530

    update cros origin

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://web-socket-frontend.vercel.app/",
+    // origin: "https://web-socket-frontend.vercel.app/",
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true,
   },
